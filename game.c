3b5155ce0113b61efaed5589542e9beab8409c61
HenrikJustUR <heju1412@gmail.com>
2026-07-12 18:18:59 +0200

    upade

--- game.c
+++ game.c
@@ -1,48 +1,61 @@
 #include "game.h"
 #include "uart.h"
 #include <avr/io.h>
+#include <stdlib.h>
 
 static int8_t player_hp;
 static int8_t enemy_hp;
 
-void game_init(void) {
+void game_init(void)
+{
 	player_hp = PLAYER_MAX_HP;
-	enemy_hp  = ENEMY_MAX_HP;
+	enemy_hp = ENEMY_MAX_HP;
 	uart_print("\r\n=== POKEBATTLE START ===\r\n");
 	uart_print("Your HP: 120 | Enemy HP: 120\r\n");
 }
 
-void game_player_turn(void) {
-	// 10% Chance zu verfehlen (TCNT0 l�uft frei)
-	if (TCNT0 % 10 < 1) {
+void game_player_turn(void)
+{
+	// FIX 2: rand() statt deterministischem TCNT0 nutzen
+	// 10% Chance zu verfehlen
+	if (rand() % 10 < 1)
+	{
 		uart_print("> Dein Angriff verfehlt!\r\n");
 		return;
 	}
 	enemy_hp -= 15;
-	if (enemy_hp < 0) enemy_hp = 0;
+	if (enemy_hp < 0)
+		enemy_hp = 0;
 	uart_print("> Treffer! 15 Schaden. Gegner HP: ");
 	uart_print_int(enemy_hp);
 	uart_print("\r\n");
 }
 
-void game_enemy_turn(void) {
+void game_enemy_turn(void)
+{
+	// FIX 2: rand() statt deterministischem TCNT0 nutzen
 	// 50% Chance zu verfehlen
-	if ((TCNT0 & 0x01) == 0) {
+	if (rand() % 2 == 0)
+	{
 		uart_print("> Gegner verfehlt!\r\n");
 		return;
 	}
 	player_hp -= 15;
-	if (player_hp < 0) player_hp = 0;
+	if (player_hp < 0)
+		player_hp = 0;
 	uart_print("> Gegner trifft! 15 Schaden. Deine HP: ");
 	uart_print_int(player_hp);
 	uart_print("\r\n");
 }
 
-GameResult game_is_over(void) {
-	if (enemy_hp  <= 0) return WIN;
-	if (player_hp <= 0) return LOSE;
+GameResult game_is_over(void)
+{
+	if (enemy_hp <= 0)
+		return WIN;
+	if (player_hp <= 0)
+		return LOSE;
 	return ONGOING;
 }
 
 int8_t game_player_hp(void) { return player_hp; }
-int8_t game_enemy_hp(void)  { return enemy_hp;  }
+int8_t game_enemy_hp(void) { return enemy_hp; }
--- main.c
+++ main.c
@@ -4,44 +4,53 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 #include "uart.h"
 #include "matrix.h"
 #include "game.h"
 
 #define BAUD 9600
-#define MYUBRR F_CPU / 8 / BAUD - 1
 
-static void adc_init(void) {
-	ADMUX  = (1 << REFS0) | (1 << MUX1);
+static void adc_init(void)
+{
+	ADMUX = (1 << REFS0) | (1 << MUX1);
 	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
-static uint16_t adc_read(void) {
+static uint16_t adc_read(void)
+{
 	ADCSRA |= (1 << ADSC);
-	while (ADCSRA & (1 << ADSC));
+	while (ADCSRA & (1 << ADSC))
+		;
 	return ADC;
 }
 
-int main(void) {
-	uart_init(MYUBRR);
+int main(void)
+{
+	// FIX 1: Direkte Übergabe der Baudrate anstatt des vorberechneten UBRR-Wertes
+	uart_init(BAUD);
 	matrix_init();
 	adc_init();
-	game_init();
 
-	// Timer0 fr Zufall starten
-	TCCR0B |= (1 << CS00);
+	// Initialer Seed für rand() aus analogem Rauschen
+	srand(adc_read());
+	game_init();
 
 	uint16_t debounce_counter = 0;
 	uint8_t action_executed = 0;
 
-	while (1) {
+	while (1)
+	{
 		uint16_t x_val = adc_read();
 
-		if (x_val > 800 || x_val < 200) {
-			if (!action_executed && game_is_over() == ONGOING) {
+		if (x_val > 800 || x_val < 200)
+		{
+			if (!action_executed && game_is_over() == ONGOING)
+			{
 				game_player_turn();
-				if (game_is_over() == ONGOING) {
+				if (game_is_over() == ONGOING)
+				{
 					game_enemy_turn();
 				}
 				action_executed = 1;
@@ -49,9 +58,11 @@ int main(void) {
 			}
 		}
 
-		if (action_executed) {
+		if (action_executed)
+		{
 			debounce_counter++;
-			if (debounce_counter > 150 && x_val > 400 && x_val < 600) {
+			if (debounce_counter > 150 && x_val > 400 && x_val < 600)
+			{
 				action_executed = 0;
 			}
 		}
